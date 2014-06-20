12b1bd32aa93313e21f99c52cb80e8e37553486d
jiamin.miao <jiamin.miao@amlogic.com>
2014-06-20 14:50:47 +0800

    It has 8723as code in trunk already,so needn't add another by difference name.
    Revert "PD# 87129: Add rtk8723 support"
    
    This reverts commit 756c44f99a2a4e7232e572362b83015266676741.

--- libbt-vendor/rtl8723as/Android.mk
+++ libbt-vendor/rtl8723as/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(BOARD_HAVE_BLUETOOTH_RTK),)
-ifeq ($(BLUETOOTH_MODULE),rtl8723au)
+ifeq ($(BLUETOOTH_MODULE),rtl8723)
 include $(CLEAR_VARS)
 
 BDROID_DIR := $(TOP_DIR)external/bluetooth/bluedroid
